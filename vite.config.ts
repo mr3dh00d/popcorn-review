be7ec58cb00618dbd07e8468f581592c094dec1d
Dereck Pavez <dereck.pavez@outlook.com>
2024-01-04 01:42:56 -0300

    Nuevas librerías y configuraciones de paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      // The key must start and end with a slash
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/assets/styles'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
+    }
+  },
+  plugins: [react()]
 })
