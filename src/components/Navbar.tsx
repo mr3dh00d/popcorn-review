a83b81681a87ea22971b81625e626d5db25013ec
Dereck Pavez <dereck.pavez@outlook.com>
2024-01-05 00:52:46 -0300

    Botón de Cerrar sesión

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 // import '@styles/navbar.css';
 import { routes } from '@/routes/routes';
 import logo from '@images/popcorn-logo.jpg';
+import { useState } from 'react';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 // import { AiFillHome, AiFillFolderOpen } from "react-icons/ai";
 
@@ -10,6 +12,7 @@ interface NavbarProps {}
  */
 // @ts-ignore
 function Navbar(props: NavbarProps) {
+  const [submenu, setSubmenu] = useState(false);
   return (
     <nav className="w-full bg-white flex justify-center h-20 fixed top-0 z-50">
       <div className="w-full max-w-screen-xl grid grid-cols-2 justify-between items-center">
@@ -28,13 +31,22 @@ function Navbar(props: NavbarProps) {
                 Mis reseñas
               </Link>
           </li>
-          <li>
-            <Link to={routes.login}>
-              <img className="rounded-full" width={40} height={40} src="/avatars/avatar_1.png" alt="avatar" />
-            </Link>
+          <li className="relative">
+            <div className="flex items-center gap-2" onClick={() => setSubmenu(!submenu)}>
+                <img className="rounded-full" width={40} height={40} src="/avatars/avatar_1.png" alt="avatar" />
+              {submenu ? <FaChevronUp /> : <FaChevronDown />}
+            </div>
+            { submenu &&
+              <div className="w-52 mt-2 absolute right-0 bg-white border flex py-2 rounded-md shadow-md">
+                <Link to={routes.base} className="w-full p-2 hover:bg-gray-300">
+                  Cerrar Sesión
+                </Link>
+              </div>
+            }
           </li>
         </ul>
       </div>
+
     </nav>
   );
 }
